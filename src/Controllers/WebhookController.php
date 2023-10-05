bc793fdcd99679f2350fd6f9e5a90ce1516bbc83
abirajs <novalnetchennai@gmail.com>
2023-10-05 13:06:51 +0530

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -537,24 +537,23 @@ class WebhookController extends Controller
         $webhookComments = sprintf($this->paymentHelper->getTranslatedText('webhook_instalment', $this->orderLanguage), date('d.m.Y'), date('H:i:s'));
         // Insert the updated instalment details into Novalnet DB
         $this->paymentService->insertPaymentResponse($this->eventData);
-        // Booking Message
+        $this->getLogger(__METHOD__)->error('Novalnet::handleInstalment', $this->eventData);
+        
+		if(isset($this->eventData['instalment']['pending_cycles'])) {
+			$nextCycleDate    = (!empty($this->eventData['instalment']['next_cycle_date'])) ? 'The next cycle date is : ' . $this->eventData['instalment']['next_cycle_date'] : '';
+			$webhookComments .=  PHP_EOL . 'Instalment Information : ' .  PHP_EOL ;
+			$webhookComments .= 'Executed_cycles : ' . $this->eventData['instalment']['cycles_executed'] . PHP_EOL;
+			$webhookComments .= 'Pending_cycles : ' . $this->eventData['instalment']['pending_cycles'] . PHP_EOL;
+			if(!empty($nextCycleDate)) {
+			$webhookComments .= $nextCycleDate . PHP_EOL ;
+			}
+			$webhookComments .= 'instalment_cycle_amount : ' . $this->eventData['instalment']['cycle_amount'] / 100 . $this->eventData['instalment']['currency'] . PHP_EOL ;
+		}
+		// Booking Message
         $this->eventData['bookingText'] = $webhookComments;
         // Create the payment to the plenty order
         $this->paymentHelper->createPlentyPayment($this->eventData);
         $this->sendWebhookMail($webhookComments);
-        $this->getLogger(__METHOD__)->error('Novalnet::handleInstalment', $this->eventData);
-        
-	if(isset($this->eventData['instalment']['pending_cycles'])) {
-		$nextCycleDate    = (!empty($this->eventData['instalment']['next_cycle_date'])) ? 'The next cycle date is : ' . $this->eventData['instalment']['next_cycle_date'] : '';
-		$webhookComments .=  PHP_EOL . 'Instalment Information : ' .  PHP_EOL ;
-		$webhookComments .= 'Executed_cycles : ' . $this->eventData['instalment']['cycles_executed'] . PHP_EOL;
-		$webhookComments .= 'Pending_cycles : ' . $this->eventData['instalment']['pending_cycles'] . PHP_EOL;
-		if(!empty($nextCycleDate)) {
-		$webhookComments .= $nextCycleDate . PHP_EOL ;
-		}
-		$webhookComments .= 'instalment_cycle_amount : ' . $this->eventData['instalment']['cycle_amount'] / 100 . $this->eventData['instalment']['currency'] . PHP_EOL ;
-	}
-		
         return $this->renderTemplate($webhookComments);
         
     }
