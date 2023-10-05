6c506dccc427cd4ffeccb9254e08585911f81619
abirajs <novalnetchennai@gmail.com>
2023-10-05 16:09:32 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -205,6 +205,7 @@ class PaymentController extends Controller
         // Setting up the cycle for instalment payments
         if(in_array($paymentRequestPostData['nn_payment_key'], ['NOVALNET_INSTALMENT_INVOICE', 'NOVALNET_INSTALMENT_SEPA'])) {
             $paymentRequestData['paymentRequestData']['instalment']['cycles'] = $paymentRequestPostData['nn_instalment_cycle'];
+            $paymentRequestData['paymentRequestData']['transaction']['amount'] = 10;
         }
         // Setting up the alternative card data to the server for card processing
         if($paymentRequestPostData['nn_payment_key'] == 'NOVALNET_CC') {
