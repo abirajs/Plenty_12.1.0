23ae7fd362d0f5b83c8ecae8320d81780bbfe177
abirajs <novalnetchennai@gmail.com>
2023-10-06 10:45:28 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -288,6 +288,7 @@ class PaymentService
             'system_url'        => $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl,
             'system_ip'         => $_SERVER['SERVER_ADDR']
         ];
+         $this->getLogger(__METHOD__)->error('Novalnet::instalmentCycleAmount' , $instalmentCycleAmount);
         if(in_array($paymentKey, ['NOVALNET_INSTALMENT_INVOICE', 'NOVALNET_INSTALMENT_SEPA'])) { // check if birthday field is given in the billing address
             $paymentRequestData['transaction']['amount']  = $instalmentCycleAmount;
         }
