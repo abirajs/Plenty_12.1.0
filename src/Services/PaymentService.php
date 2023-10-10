54740af41a2aa8546792fcdf785225a0efbe10e5
abirajs <novalnetchennai@gmail.com>
2023-10-10 18:05:26 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1132,7 +1132,7 @@ class PaymentService
 			if(!empty($nextCycleDate)) {
 			$InstalmentComments .= $nextCycleDate . PHP_EOL ;
 			}
-			$InstalmentComments .= 'instalment_cycle_amount : ' .  $this->paymentHelper->convertAmountToSmallerUnit($transactionData['cycle_amount']) / 100  . '  ' . $transactionData['currency'] . PHP_EOL ;
+			$InstalmentComments .= 'instalment_cycle_amount : ' .  $transactionData['cycle_amount'] / 100 . '  ' . $transactionData['currency'] . PHP_EOL ;
 		}
         return $InstalmentComments;
     }
