37fd81edaf7b96db04b8c8d798641c8345bfb757
abirajs <novalnetchennai@gmail.com>
2023-10-13 20:00:27 +0530

    Add files via upload

---
+++ resources/js/novalnetInstalmentSepa.js
@@ -0,0 +1,58 @@
+jQuery(document).ready( function() {
+    // Restrict the special characters in the IBAN field
+    jQuery('#nn_sepa_iban').on('input',function ( event ) {
+        let iban = jQuery(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
+        jQuery(this).val(iban);
+    });
+    // After the form submission disable the action
+    jQuery('#novalnet_form').on('submit',function() {
+        jQuery('#novalnet_form_btn').prop('disabled', true);
+        if(jQuery('#nn_show_birthday').val() == true && (jQuery("#nn_instalment_year").val() == '' || jQuery("#nn_instalment_date").val() == '' || jQuery("#nn_instalment_month").val() == '0')) {
+           jQuery('#novalnet_form_btn').prop('disabled', false);
+        }
+        // Validate the BIC when if it is mandatory
+        var ibanCountry = jQuery('#nn_sepa_iban').val().substring(0,2);
+        if (ibanCountry.match(/(?:CH|MC|SM|GB)$/) && jQuery('#nn_sepa_bic').val() == '') {
+            alert(jQuery('#nn_account_data_invalid').val());
+            jQuery('#novalnet_form_btn').prop('disabled', false);
+            return false;
+        }
+    });
+    
+      
+function formatMoney(amount, decimalCount = 2, decimal = ",", thousands = ".") {
+  try {
+    decimalCount = Math.abs(decimalCount);
+    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+    const negativeSign = amount < 0 ? "-" : "";
+    let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+    let j = (i.length > 3) ? i.length % 3 : 0;
+    return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "jQuery1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+  } catch (e) {
+    console.log(e)
+  }
+}
+});
+
+
+jQuery('#nn_instalment_cycle').on('change',function() {  
+var selectedOption = this.options[this.selectedIndex];
+var selectedValue = selectedOption.value;
+var parts = selectedValue.split("-"); // Split the value into key and value
+
+var key = parts[0];
+var value = parts[1];
+
+var cycleInformation = '';              
+for (instalmentCycle = 1; instalmentCycle <= key; instalmentCycle++) {
+	if(instalmentCycle != key)
+	{
+		cycleInformation += '<tr><td>' + instalmentCycle + '</td><td>'+jQuery(this).find(':selected').attr('data-amount') +'</td></tr>';
+	} else {
+		var lastCycleAmount = (jQuery('#nn_net_amount').val() - (jQuery(this).find(':selected').attr('data-cycle-amount') * (key - 1)));
+		cycleInformation += '<tr><td>' + instalmentCycle + '</td><td>'+ formatMoney(lastCycleAmount) + ' '+ jQuery('#nn_order_currency').val()+'</td></tr>';
+	}
+}                           
+jQuery('#nn_instalment_cycle_information').html(cycleInformation);
+}).change();
+
