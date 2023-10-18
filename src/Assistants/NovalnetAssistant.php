70ca6503c477d0be25fd0431dd5cedd24de7716d
abirajs <novalnetchennai@gmail.com>
2023-10-18 16:34:45 +0530

    Update NovalnetAssistant.php

--- src/Assistants/NovalnetAssistant.php
+++ src/Assistants/NovalnetAssistant.php
@@ -624,9 +624,10 @@ class NovalnetAssistant extends WizardProvider
                 $nnInstalmentPayment .  'instamentCycles' =>
                 [
                        'type' => 'checkboxGroup',
-                       'defaultValue' => '2','3','4','5','6','7','8','9','10','11','12','15','18','21','24',
+                       'defaultValue' => ['2'],['3'],['4'],['5'],['6'],['7'],['8'],['9'],['10'],['11'],['12'],['15'],['18'],['21'],['24'],
                        'options' => [
                            'name' => 'NovalnetAssistant.novalnetinstamentCyclesLabel',
+                            'required'  => true,
                            'checkboxValues' =>
                            [
                                 [
