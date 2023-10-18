be1b2b6767ab3208bb4746f54ec3e910c5de8b14
abirajs <novalnetchennai@gmail.com>
2023-10-18 15:40:38 +0530

    Update PaymentHelper.php

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -413,6 +413,7 @@ class PaymentHelper
      */
     public function createPlentyPayment($paymentResponseData)
     {
+         $this->getLogger(__METHOD__)->alert('Novalnet::createPlentyPayment001', $paymentResponseData);
         try {
             /** @var Payment $payment */
             $payment = pluginApp(\Plenty\Modules\Payment\Models\Payment::class);
