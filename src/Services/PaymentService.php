9b0c6708c240e98050c7e39807a6b4a0866c614f
abirajs <novalnetchennai@gmail.com>
2023-10-19 11:00:04 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1001,7 +1001,7 @@ class PaymentService
         }
         // Form the bank details for invoice payments
         if((in_array($transactionData['paymentName'], ['novalnet_invoice', 'novalnet_prepayment']) && !in_array($transactionData['tx_status'], ['DEACTIVATED', 'FAILURE'])) || (in_array($transactionData['paymentName'], ['novalnet_guaranteed_invoice', 'novalnet_instalment_invoice']) && !in_array($transactionData['tx_status'], ['PENDING', 'DEACTIVATED', 'FAILURE']))) {
-            $transactionComments .= PHP_EOL . $this->getBankDetailsInformation($transactionData);
+            $transactionComments .= PHP_EOL . $this->Information($transactionData);
         }
         // Form the instalment details for instalment payments
         if((in_array($transactionData['paymentName'], ['novalnet_instalment_sepa', 'novalnet_instalment_invoice']) && !in_array($transactionData['tx_status'], ['PENDING', 'DEACTIVATED', 'FAILURE']))) {
@@ -1062,6 +1062,7 @@ class PaymentService
      */
     public function getBankDetailsInformation($transactionData)
     {
+	     $this->getLogger(__METHOD__)->error('Novalnet::getBankDetailsInformation ', $transactionData);
 	if(in_array($transactionData['paymentName'], ['novalnet_instalment_invoice', 'novalnet_instalment_sepa'])) {
 		$invoiceComments = PHP_EOL . sprintf($this->paymentHelper->getTranslatedText('transfer_amount_duedate_text'), $transactionData['cycle_amount']/100, $transactionData['currency'], date('Y/m/d', (int)strtotime($transactionData['due_date'])));
         // If the transaction is in On-Hold not displaying the due date
