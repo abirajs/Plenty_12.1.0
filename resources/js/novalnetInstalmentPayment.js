818f7172383342c88ede47f691afad10659f277b
abirajs <novalnetchennai@gmail.com>
2023-10-20 13:37:56 +0530

    Update novalnetInstalmentPayment.js

--- resources/js/novalnetInstalmentPayment.js
+++ resources/js/novalnetInstalmentPayment.js
@@ -23,7 +23,7 @@ jQuery('#nn_instalment_cycle_information').html(cycleInformation);
 }).change();
     
 jQuery(document).ready( function() {
-    if(jQuery("#nn_instalment_date").val() != '') {
+    if(jQuery("#nn_validation").val() == '') {
     var current_date = new Date();
     var max_year = current_date.getFullYear() - 18;
     var min_year = current_date.getFullYear() - 91;
