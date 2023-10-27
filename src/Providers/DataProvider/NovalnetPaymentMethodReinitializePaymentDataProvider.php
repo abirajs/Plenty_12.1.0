efec1b11e05227e284279b71196e045e72644488
abirajs <novalnetchennai@gmail.com>
2023-10-27 18:32:58 +0530

    Update NovalnetPaymentMethodReinitializePaymentDataProvider.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
@@ -107,7 +107,7 @@ class NovalnetPaymentMethodReinitializePaymentDataProvider
                                      ];
                  }
                 $this->getLogger(__METHOD__)->error('Novalnet:: $order',  $order);
-                $this->getLogger(__METHOD__)->error('Novalnet:: $orderAmountcurrency',   $this->sessionStorage->getPlugin()->getValue('orderCurency'););
+                $this->getLogger(__METHOD__)->error('Novalnet:: $orderAmountcurrency',   $this->sessionStorage->getPlugin()->getValue('orderCurency'));
                 $this->getLogger(__METHOD__)->error('Novalnet:: $basketRepository->load()',  $basketRepository->load());
                 $this->getLogger(__METHOD__)->error('Novalnet:: $basketRepository->load()currency',  $basketRepository->load()->currency);
                 // Check if the birthday field needs to show for guaranteed and instalment payments
@@ -143,7 +143,7 @@ class NovalnetPaymentMethodReinitializePaymentDataProvider
                                     'redirectUrl' => $paymentService->getRedirectPaymentUrl(),
                                     'orderLang'   => $paymentRequestData['paymentRequestData']['custom']['lang'],
                                     'countryCode' => $paymentRequestData['paymentRequestData']['customer']['billing']['country_code'],
-                                    'orderCurrency'  =>  $this->sessionStorage->getPlugin()->getValue('orderCurency');,
+                                    'orderCurrency'  =>  $this->sessionStorage->getPlugin()->getValue('orderCurency'),
                                     'googlePayData' => !empty($googlePayData) ? $googlePayData : '',
                                     'Currency'  => $currency,
                                     'AccountHolderName'     => $paymentRequestData['paymentRequestData']['customer']['first_name'] . ' ' . $paymentRequestData['paymentRequestData']['customer']['last_name'],
