d69c0ba47c891cbcd08b6087a511d4467965c044
abirajs <novalnetchennai@gmail.com>
2023-10-27 19:00:53 +0530

    Update NovalnetPaymentMethodReinitializePaymentDataProvider.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePaymentDataProvider.php
@@ -64,7 +64,6 @@ class NovalnetPaymentMethodReinitializePaymentDataProvider
                 // Get the proper order amount even the system currency and payment currency are differ
                 if(count($order['amounts']) > 1) {
                      foreach($order['amounts'] as $orderAmount) {
-                         $sessionStorage->getPlugin()->setValue('orderCurency', $orderAmount['currency']);
                         if($basketRepository->load()->currency == $orderAmount['currency']) {
                             $invoiceAmount = $paymentHelper->convertAmountToSmallerUnit($orderAmount['invoiceTotal']);
                         }
