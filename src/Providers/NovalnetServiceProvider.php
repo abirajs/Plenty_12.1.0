27bf21123b905ecf708efbc4cf5bfb92d400c8af
abirajs <novalnetchennai@gmail.com>
2023-10-28 14:19:52 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -141,14 +141,14 @@ class NovalnetServiceProvider extends ServiceProvider
                 if($paymentKey) {
                     $sessionStorage->getPlugin()->setValue('orderCurency', null);
                     $paymentRequestData = $paymentService->generatePaymentParams($basketRepository->load(), $paymentKey);
+
+                if((empty($paymentRequestData['paymentRequestData']['customer']['first_name']) && empty($paymentRequestData['paymentRequestData']['customer']['last_name'])) || empty($paymentRequestData['paymentRequestData']['customer']['email'])) {
+                    $content = $paymentHelper->getTranslatedText('nn_first_last_name_error');
+                    $contentType = 'errorCode';
                    if(empty($paymentRequestData['paymentRequestData']['customer']['email'])){
                     $content = 'Email is missing';
                     $contentType = 'errorCode';   
                     }
-                if(empty($paymentRequestData['paymentRequestData']['customer']['first_name']) && empty($paymentRequestData['paymentRequestData']['customer']['last_name'])) {
-                    $content = $paymentHelper->getTranslatedText('nn_first_last_name_error');
-                    $contentType = 'errorCode';
-
                 } else {
                     // Check if the birthday field needs to show for guaranteed payments
                      $showBirthday = ($settingsService->getPaymentSettingsValue('allow_b2b_customer', strtolower($paymentKey)) == false || (!isset($paymentRequestData['paymentRequestData']['customer']['billing']['company']) && !isset($paymentRequestData['paymentRequestData']['customer']['birth_date'])) ||  (isset($paymentRequestData['paymentRequestData']['customer']['birth_date']) && time() < strtotime('+18 years', strtotime($paymentRequestData['paymentRequestData']['customer']['birth_date'])))) ? true : false;
