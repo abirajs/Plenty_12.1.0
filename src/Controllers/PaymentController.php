fcc4301e45a85e5696719291a9e1f1e631f03ef9
abirajs <novalnetchennai@gmail.com>
2023-10-30 12:06:38 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -274,11 +274,11 @@ class PaymentController extends Controller
 	$paymentRequestData = $this->sessionStorage->getPlugin()->getValue('nnPaymentData');
 	if((empty($paymentRequestData['paymentRequestData']['customer']['first_name']) && empty($paymentRequestData['paymentRequestData']['customer']['last_name'])) || empty($paymentRequestData['paymentRequestData']['customer']['email'])) {
 		$content = $paymentHelper->getTranslatedText('nn_first_last_name_error');
-		$this->pushNotification($content, 'error', 100);
+		 $this->paymentService->pushNotification($content, 'error', 100);
 		return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
 	   if(empty($paymentRequestData['paymentRequestData']['customer']['email'])){
 		$content = $paymentHelper->getTranslatedText('nn_email_error');
-		$this->pushNotification($content, 'error', 100);
+		 $this->paymentService->pushNotification($content, 'error', 100);
 		return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');  
 		}
 	} 
