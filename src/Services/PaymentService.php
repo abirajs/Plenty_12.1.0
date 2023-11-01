a06f12ec5c57790eebd091c1d87aa6743981177a
abirajs <novalnetchennai@gmail.com>
2023-11-01 16:21:51 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1029,6 +1029,7 @@ class PaymentService
      */
     public function formTransactionComments($transactionData)
     {
+	$this->getLogger(__METHOD__)->error('Novalnet::formTransactionComments ', $transactionData); 
         $transactionComments = '';
         // Display the Novalnet transaction Id
         if(!empty($transactionData['tid'])) {
@@ -1381,6 +1382,7 @@ class PaymentService
    public function getSavedPaymentDetails(&$paymentResponseData)
    {
        $transactionData = $this->getDatabaseValues($paymentResponseData['transaction']['order_no']);
+       $this->getLogger(__METHOD__)->error('Novalnet::getSavedPaymentDetails ', $transactionData); 
        if(in_array($transactionData['paymentName'], ['novalnet_invoice', 'novalnet_guaranteed_invoice', 'novalnet_prepayment', 'novalnet_instalment_invoice'])) {
            $paymentResponseData['transaction']['bank_details']['account_holder'] = $transactionData['invoice_account_holder'];
            $paymentResponseData['transaction']['bank_details']['iban']           = $transactionData['invoice_iban'];
