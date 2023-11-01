3296ac41ce876e2c9bdfb3c999866126cb4a9a50
abirajs <novalnetchennai@gmail.com>
2023-11-01 09:40:35 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -319,6 +319,7 @@ class PaymentService
 		}
             }
         }
+	$sessionStorage->getPlugin()->setValue('orderCurency',null);  
         // Build the custom parameters
         $paymentRequestData['custom'] = ['lang'  => strtoupper($this->sessionStorage->getLocaleSettings()->language)];
         // Build additional specific payment method request parameters
