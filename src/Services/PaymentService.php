6181bcf0cb0730863b7f619f38d0be00522f07a0
abirajs <novalnetchennai@gmail.com>
2023-11-02 18:33:26 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -514,7 +514,7 @@ class PaymentService
 		
 		
 		if(in_array($paymentRequestData['paymentRequestData']['transaction']['payment_type'], ['NOVALNET_GUARANTEED_INVOICE', 'NOVALNET_GUARANTEED_SEPA', 'NOVALNET_INSTALMENT_INVOICE', 'NOVALNET_INSTALMENT_SEPA'])) {
-			if(isset($paymentRequestData['paymentRequestData']['customer']['billing']['same_as_billing'] || $paymentRequestData['paymentRequestData']['transaction']['currency']) {
+			if(!isset($paymentRequestData['paymentRequestData']['customer']['billing']['same_as_billing']) || $paymentRequestData['paymentRequestData']['transaction']['currency'] != 'EUR') {
 				$content = $this->paymentHelper->getTranslatedText('nn_email_error');
 				$this->pushNotification($content, 'error', 100);	
 				return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
