656bda7c0065691c357683227e083ff642b5e6a6
abirajs <novalnetchennai@gmail.com>
2023-11-02 19:27:37 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -501,7 +501,7 @@ class PaymentService
         
         if(in_array($paymentRequestData['paymentRequestData']['transaction']['payment_type'], ['NOVALNET_GUARANTEED_INVOICE', 'NOVALNET_GUARANTEED_SEPA'])) {
         if($this->isGuaranteePaymentToBeDisplayed( $this->basketRepository->load() , 'novalnet_guaranteed_invoice') != 'guarantee' || $this->isGuaranteePaymentToBeDisplayed( $this->basketRepository->load() , 'novalnet_guaranteed_sepa') != 'guarantee'){
-			$content = $this->paymentHelper->getTranslatedText('nn_first_last_name_error');
+			$content = $this->paymentHelper->getTranslatedText('nn_email_error');
 			$this->pushNotification($content, 'error', 100);	
 			return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
 		}
@@ -509,7 +509,7 @@ class PaymentService
 		
 		if(in_array($paymentRequestData['paymentRequestData']['transaction']['payment_type'], ['NOVALNET_INSTALMENT_INVOICE', 'NOVALNET_INSTALMENT_SEPA'])) {
         if($this->isGuaranteePaymentToBeDisplayed( $this->basketRepository->load() , 'novalnet_instalment_invoice') != 'true' || $this->isGuaranteePaymentToBeDisplayed( $this->basketRepository->load() , 'novalnet_instalment_sepa') != 'true'){
-			$content = $this->paymentHelper->getTranslatedText('nn_first_last_name_error');
+			$content = $this->paymentHelper->getTranslatedText('nn_email_error');
 			$this->pushNotification($content, 'error', 100);	
 			return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
 		}
