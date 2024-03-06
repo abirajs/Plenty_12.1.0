621d9975b426f5c6a8b1a9d82c0ea2001a483e0e
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-06 20:15:43 +0530

    Update NovalnetPaymentAbstract.php

--- src/Methods/NovalnetPaymentAbstract.php
+++ src/Methods/NovalnetPaymentAbstract.php
@@ -134,7 +134,7 @@ abstract class NovalnetPaymentAbstract extends PaymentMethodBaseService
         // $paymentMethodKey[0] = strtolower($paymentMethodKey[0]);
 
         // /** @var Translator $translator */
-        // $translator = pluginApp(Translator::class);
+        $translator = pluginApp(Translator::class);
         // return $translator->trans('Novalnet::Customize.'. $paymentMethodKey, [], $lang);
         return $translator->trans('Novalnet', [], $lang);
 
