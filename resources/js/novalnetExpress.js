d67bcce0736a7396e8747a9bd6f8be651dac514c
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-07 10:03:43 +0530

    Update novalnetExpress.js

--- resources/js/novalnetExpress.js
+++ resources/js/novalnetExpress.js
@@ -27,6 +27,28 @@ jQuery(document).ready(function() {
                 order: {
                     paymentDataPresent: false,
                     merchantName: String(jQuery('#nn_business_name').val()),
+                    billing: {
+                    	requiredFields: ["postalAddress", "phone", "email"]
+                    },
+                    shipping: {
+                    	requiredFields: ["postalAddress", "phone"],    	
+                		methods: [
+                          {
+                            identifier: "freeshipping",
+                	        amount: 0,
+                	        detail: "Free shipping within Deutschland",				
+                	        label: "Free Shipping"
+                	      },
+                	      {
+                	        identifier: "dhlshipping",
+                	        amount: 500,
+                	        detail: "The product will be delivered depends on the executive",
+                	        label: "DHL Shipping"
+                	      }
+                        ],
+                		defaultIdentifier: "dhlshipping",	
+                		methodsUpdatedLater: true
+                    }
                 },
                 button: {
                     type: jQuery('#nn_button_type').val(),
