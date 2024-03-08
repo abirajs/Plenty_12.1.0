61eba1ba8ea229a7885e31c7c23107189f19621d
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-08 14:13:41 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -385,7 +385,7 @@ class PaymentController extends Controller
         $response = $this->send_request($json_data, $endpoint, $headers);
 	     $responseArray = json_decode($response, true);
         $this->getLogger(__METHOD__)->error('Novalnet::$response', $responseArray);
-        // $this->sessionStorage->getPlugin()->setValue('nnPaymentData',$responseArray);
+        $this->sessionStorage->getPlugin()->setValue('nnPaymentData',$responseArray);
 	    // $this->paymentService->HandlePaymentResponse();
 	    // return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
          return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/place-order');
