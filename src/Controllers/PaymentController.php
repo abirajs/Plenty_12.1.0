94c7862614191898141d6ac14119f9efb7cfa461
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-08 10:21:47 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -309,7 +309,7 @@ class PaymentController extends Controller
         // Get the payment form post data
         $paymentRequestPostData = $this->request->all();
         $test = json_decode(json_encode($paymentRequestPostData['nn_google_pay_response']));
-        $array = json_decode($jsonString, true);
+        $array = json_decode($test, true);
         $this->getLogger(__METHOD__)->error('Novalnet::$paymentRequestPostData', $paymentRequestPostData);
         $this->getLogger(__METHOD__)->error('Novalnet::$array', $array);
         return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/place-order');
