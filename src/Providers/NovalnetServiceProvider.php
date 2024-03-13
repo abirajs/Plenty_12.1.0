e09f29466c28ca7f2391993ea251cf899a6d4213
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-13 10:44:21 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -204,7 +204,7 @@ class NovalnetServiceProvider extends ServiceProvider
                 }
                 if($sessionStorage->getPlugin()->getValue('test') == 'test') {
                      // $paymentService->getProcessPaymentUrl();
-                    // return $response->redirectTo('/payment/novalnet/processPayment/');
+                    return $response->redirectTo('/payment/novalnet/processPayment/');
                     $this->getLogger(__METHOD__)->error('Novalnet::null null', 'null');
                     // return $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/' . $sessionStorage->getLocaleSettings()->language . '/payment/novalnet/processPayment/';
                      
