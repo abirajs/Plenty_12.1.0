327e740aabacefac9354cbd44d4f5d641d85d830
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-14 18:40:17 +0530

    Update NovalnetExpressCheckoutApplePayDataProvider.php

--- src/Providers/DataProvider/NovalnetExpressCheckoutApplePayDataProvider.php
+++ src/Providers/DataProvider/NovalnetExpressCheckoutApplePayDataProvider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is used for displaying the Google Pay button
+ * This file is used for displaying the Apple Pay button
  *
  * @author       Novalnet AG
  * @copyright(C) Novalnet
@@ -28,7 +28,7 @@ class NovalnetExpressCheckoutApplePayDataProvider
 {
     use Loggable;
     /**
-     * Display the Google Pay button
+     * Display the Apple Pay button
      *
      * @param Twig $twig
      * @param BasketRepositoryContract $basketRepository
@@ -50,7 +50,7 @@ class NovalnetExpressCheckoutApplePayDataProvider
         $paymentService     = pluginApp(PaymentService::class);
         $settingsService    = pluginApp(SettingsService::class);
         $this->getLogger(__METHOD__)->error('Novalnet::ExpressBasket failed', $basket);
-        if($settingsService->getPaymentSettingsValue('payment_active', 'novalnet_googlepay') == true) {
+        if($settingsService->getPaymentSettingsValue('payment_active', 'novalnet_applepay') == true) {
             if(!empty($basket->basketAmount)) {
                 $orderAmount = 0;
                 /** @var \Plenty\Modules\Frontend\Services\VatService $vatService */
@@ -66,31 +66,31 @@ class NovalnetExpressCheckoutApplePayDataProvider
                 $orderAmount = $paymentHelper->convertAmountToSmallerUnit($basket->basketAmount);
             }
             // Get the Payment MOP Id
-            $paymentMethodDetails = $paymentHelper->getPaymentMethodByKey('NOVALNET_GOOGLEPAY');
+            $paymentMethodDetails = $paymentHelper->getPaymentMethodByKey('NOVALNET_APPLEPAY');
             // Get the order language
             $orderLang = strtoupper($sessionStorage->getLocaleSettings()->language);
             // Get the countryCode
             $billingAddress = $paymentHelper->getCustomerAddress((int) $basket->customerInvoiceAddressId);
             // Get the seller name from the shop configuaration
-            $sellerName = $settingsService->getPaymentSettingsValue('business_name', 'novalnet_googlepay');
-            // Required details for the Google Pay button
-            $googlePayData = [
+            $sellerName = $settingsService->getPaymentSettingsValue('business_name', 'novalnet_applepay');
+            // Required details for the Apple Pay button
+            $applePayData = [
                                 'clientKey'     => trim($settingsService->getPaymentSettingsValue('novalnet_client_key')),
-                                'merchantId'    => $settingsService->getPaymentSettingsValue('payment_active', 'novalnet_googlepay'),
+                                'merchantId'    => $settingsService->getPaymentSettingsValue('payment_active', 'novalnet_applepay'),
                                 'sellerName'    => !empty($sellerName) ? $sellerName : $webstoreHelper->getCurrentWebstoreConfiguration()->name,
-                                'enforce'       => $settingsService->getPaymentSettingsValue('enforce', 'novalnet_googlepay'),
-                                'buttonType'    => $settingsService->getPaymentSettingsValue('button_type', 'novalnet_googlepay'),
-                                'buttonHeight'  => $settingsService->getPaymentSettingsValue('button_height', 'novalnet_googlepay'),
-                                'testMode'      => ($settingsService->getPaymentSettingsValue('test_mode', 'novalnet_googlepay') == true) ? 'SANDBOX' : 'PRODUCTION'
+                                'enforce'       => $settingsService->getPaymentSettingsValue('enforce', 'novalnet_applepay'),
+                                'buttonType'    => $settingsService->getPaymentSettingsValue('button_type', 'novalnet_applepay'),
+                                'buttonHeight'  => $settingsService->getPaymentSettingsValue('button_height', 'novalnet_applepay'),
+                                'testMode'      => ($settingsService->getPaymentSettingsValue('test_mode', 'novalnet_applepay') == true) ? 'SANDBOX' : 'PRODUCTION'
                              ];
-            $this->getLogger(__METHOD__)->error('Novalnet::$googlePayData', $googlePayData);
+            $this->getLogger(__METHOD__)->error('Novalnet::$applePayData', $applePayData);
             $this->getLogger(__METHOD__)->error('Novalnet::$paymentMethodDetails[0]', $paymentMethodDetails[0]);
             $this->getLogger(__METHOD__)->error('Novalnet::$paymentService->getProcessPaymentUrl()', $paymentService->getExpressPaymentUrl());
-            // Render the Google Pay button
+            // Render the Apple Pay button
             return $twig->render('Novalnet::PaymentForm.NovalnetExpressCheckoutApplePay',
                                         [
                                             'paymentMethodId'       => $paymentMethodDetails[0],
-                                            'googlePayData'         => $googlePayData,
+                                            'applePayData'          => $applePayData,
                                             'countryCode'           => !empty($countryRepository->findIsoCode($billingAddress->countryId, 'iso_code_2')) ? $countryRepository->findIsoCode($billingAddress->countryId, 'iso_code_2') : 'DE',
                                             'orderAmount'           => $orderAmount,
                                             'orderLang'             => $orderLang,
