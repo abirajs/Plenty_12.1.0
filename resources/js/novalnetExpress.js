4fde33d01c53c293d9b91a7d773430e4566e1e96
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-14 11:54:49 +0530

    Update novalnetExpress.js

--- resources/js/novalnetExpress.js
+++ resources/js/novalnetExpress.js
@@ -18,7 +18,7 @@ console.log('Plentymarket Domain:', plentymarketDomain);
                 },
                 transaction: {
                     setPendingPayment: true,
-                    amount: String(jQuery('#nn_google_pay').attr('data-total-amount')),
+                    amount: (String(jQuery('#nn_google_pay').attr('data-total-amount')) != '') ? String(jQuery('#nn_google_pay').attr('data-total-amount')) : ((window.ceresStore.state.items[window.ceresStore.state.items.mainItemId].variation.documents[0].data.prices.default.price.value).toFixed(2)) * jQuery('.add-to-basket-container').find('input[type="text"], input[type="number"]').first().val() * 100,
                     currency: String(jQuery('#nn_google_pay').attr('data-currency')),
                     enforce3d: Boolean(jQuery('#nn_enforce').val()),
                     paymentMethod: "GOOGLEPAY",
@@ -144,7 +144,9 @@ console.log('Plentymarket Domain:', plentymarketDomain);
                         // } else {
                         // location.reload();
                         // }
+                        
                         jQuery('.fa-shopping-cart').parent('button').click();
+                         
                         clickResult({status: "SUCCESS"});
                     },
                 }
