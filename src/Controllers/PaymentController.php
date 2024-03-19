ff95838e973d053becb2ce70bb84233d92e077c4
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-19 19:04:56 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -473,7 +473,7 @@ class PaymentController extends Controller
 	// Set the customer shipping address ID
 	$this->checkout->setCustomerShippingAddressId($createdAddress->id);
 
-	$this->addressContract->setEnable(false);
+	$contactAddress->setEnable(false);
 
 	$payment_access_key  = $this->settingsService->getPaymentSettingsValue('novalnet_private_key');
         $encoded_data        = base64_encode($payment_access_key);
