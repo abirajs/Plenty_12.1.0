6f3cad81b9a49aa70e8a0bd6035c1affe96c800a
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-20 15:13:22 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -536,8 +536,9 @@ class PaymentController extends Controller
         ];
         $this->getLogger(__METHOD__)->error('Novalnet::$data', $data);
 	$this->sessionStorage->getPlugin()->setValue('nnExpressPaymentData',$data);
-  	return $this->response->redirectTo('checkout?readonlyCheckout=1');
-	// return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/place-order');
+	    $this->sessionStorage->getPlugin()->setValue('test','test');
+  	// return $this->response->redirectTo('checkout?readonlyCheckout=1');
+	return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/place-order');
     }
 
 
