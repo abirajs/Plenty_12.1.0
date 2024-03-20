5f0f7f10e4e62499a50519cbf7134d37b5197064
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-20 18:46:00 +0530

    Update novalnetExpressCheckout.js

--- resources/js/novalnetExpressCheckout.js
+++ resources/js/novalnetExpressCheckout.js
@@ -29,28 +29,28 @@ jQuery(document).ready(function() {
 					order: {
 						paymentDataPresent: false,
 						merchantName: configurationData[paymentTypeValue].seller_name,
-						billing: {
-							requiredFields: ["postalAddress", "phone", "email"]
-						},
-						shipping: {
-							requiredFields: ["postalAddress", "phone"],
-							methods: [
-							  {
-								identifier: "freeshipping",
-								amount: 0,
-								detail: "Free shipping within Deutschland",				
-								label: "Free Shipping"
-							  },
-							  {
-								identifier: "dhlshipping",
-								amount: 500,
-								detail: "The product will be delivered depends on the executive",
-								label: "DHL Shipping"
-							  }
-							],
-							defaultIdentifier: "dhlshipping",	
-							methodsUpdatedLater: true
-						 }
+						// billing: {
+						// 	requiredFields: ["postalAddress", "phone", "email"]
+						// },
+						// shipping: {
+						// 	requiredFields: ["postalAddress", "phone"],
+						// 	methods: [
+						// 	  {
+						// 		identifier: "freeshipping",
+						// 		amount: 0,
+						// 		detail: "Free shipping within Deutschland",				
+						// 		label: "Free Shipping"
+						// 	  },
+						// 	  {
+						// 		identifier: "dhlshipping",
+						// 		amount: 500,
+						// 		detail: "The product will be delivered depends on the executive",
+						// 		label: "DHL Shipping"
+						// 	  }
+						// 	],
+						// 	defaultIdentifier: "dhlshipping",	
+						// 	methodsUpdatedLater: true
+						//  }
 					},
 					button: {
 						type: configurationData[paymentTypeValue].button_type,
@@ -67,22 +67,13 @@ jQuery(document).ready(function() {
 							// Only on success, we proceed further with the booking
 							if(response.result.status == "SUCCESS") {
 								console.log(response);
-								// var array = json_decode(response, true);
-						
 								jQuery('#nn_google_pay_response').val(JSON.stringify(response));
 								jQuery('#nn_currency').val(jQuery('#nn_order_currency').val()); 
 								jQuery('#nn_google_pay_wallet_token').val(response.transaction.token);
 								jQuery('#nn_google_pay_do_redirect').val(response.transaction.doRedirect);                               
 								jQuery('#nn_google_pay_form').submit();
 								jQuery('#nn_google_pay').find('button').prop('disabled', true);
-							
-								// jQuery('#nn_apple_pay_response').val(JSON.stringify(response));
-								// jQuery('#nn_currency').val(jQuery('#nn_order_currency').val()); 
-								// jQuery('#nn_apple_pay_token').val(response.transaction.token);
-								// jQuery('#nn_apple_pay_do_redirect').val(response.transaction.doRedirect);                               
-								// jQuery('#nn_apple_pay_form').submit();
-								// jQuery('#nn_apple_pay').find('button').prop('disabled', true);
-								// }                                                
+                                         
 							} else {
 								// Upon failure, displaying the error text
 								if(response.result.status_text) {
@@ -92,73 +83,72 @@ jQuery(document).ready(function() {
 							}
 						},
 						
-						onShippingContactChange : function(shippingContact, newShippingContactResult) {
-						let transactionInfoToUpdate = {};
-						// There could be a situation where the shipping methods differ based on region
-						if (shippingContact.countryCode == "DE" || shippingContact.countryCode == "US") {		
-							transactionInfoToUpdate.methods = [{
-								identifier: "dhlshipping",
-								amount: 500,
-								detail: "The product will be delivered depends on the executive",
-								label: "DHL Shipping"
-							}, {
-								identifier: "freeshipping",
-								amount: 0,
-								detail: "Free shipping within Deutschland",
-								label: "Free Shipping"
-							}];
-						} else {
-							transactionInfoToUpdate.methods = [{
-								identifier: "expressshipping",
-								amount: 750,
-								detail: "The product will be dispatched in the same day",				
-								label: "Express Shipping"
-							}];
-						}
+						// onShippingContactChange : function(shippingContact, newShippingContactResult) {
+						// let transactionInfoToUpdate = {};
+						// // There could be a situation where the shipping methods differ based on region
+						// if (shippingContact.countryCode == "DE" || shippingContact.countryCode == "US") {		
+						// 	transactionInfoToUpdate.methods = [{
+						// 		identifier: "dhlshipping",
+						// 		amount: 500,
+						// 		detail: "The product will be delivered depends on the executive",
+						// 		label: "DHL Shipping"
+						// 	}, {
+						// 		identifier: "freeshipping",
+						// 		amount: 0,
+						// 		detail: "Free shipping within Deutschland",
+						// 		label: "Free Shipping"
+						// 	}];
+						// } else {
+						// 	transactionInfoToUpdate.methods = [{
+						// 		identifier: "expressshipping",
+						// 		amount: 750,
+						// 		detail: "The product will be dispatched in the same day",				
+						// 		label: "Express Shipping"
+						// 	}];
+						// }
 
-						// Recalculating the total gross based on the chosen shipping method
-						transactionInfoToUpdate.amount = transactionInfoToUpdate.methods[0].amount + requestData.paymentIntent.transaction.amount;	
-						newShippingContactResult(transactionInfoToUpdate);
-					 },
-					 onShippingMethodChange : function(shippingMethod, newShippingMethodResult) {
-						 // There could be a situation where the shipping method can alter total  
-						let transactionInfoToUpdate = {};
-						// Recalculating the total gross based on the chosen shipping method
-						transactionInfoToUpdate.amount = (parseInt(shippingMethod.amount) * 100) + requestData.paymentIntent.transaction.amount;
-						newShippingMethodResult(transactionInfoToUpdate);
-					 },
+						// // Recalculating the total gross based on the chosen shipping method
+						// transactionInfoToUpdate.amount = transactionInfoToUpdate.methods[0].amount + requestData.paymentIntent.transaction.amount;	
+						// newShippingContactResult(transactionInfoToUpdate);
+					 // },
+					 // onShippingMethodChange : function(shippingMethod, newShippingMethodResult) {
+						//  // There could be a situation where the shipping method can alter total  
+						// let transactionInfoToUpdate = {};
+						// // Recalculating the total gross based on the chosen shipping method
+						// transactionInfoToUpdate.amount = (parseInt(shippingMethod.amount) * 100) + requestData.paymentIntent.transaction.amount;
+						// newShippingMethodResult(transactionInfoToUpdate);
+					 // },
 						
-					 onPaymentButtonClicked: function(clickResult) {
-							console.log('click');
-							console.log(window.ceresStore.state);
-							console.log(window.ceresStore.state.basket.item);
-							var id = null;
-							if (typeof window.ceresStore.state.item !== 'undefined' && window.ceresStore.state.item.variation.documents[0]) {
-							id = window.ceresStore.state.item.variation.documents[0].data.variation.id;
-							} else if (typeof window.ceresStore.state.items.mainItemId !== 'undefined' && window.ceresStore.state.items[window.ceresStore.state.items.mainItemId]) {
-							id = window.ceresStore.state.items[window.ceresStore.state.items.mainItemId].variation.documents[0].data.variation.id;
-							}
-							if(walletPayments[walletPayment] == 'novalnet_applepay')  {
-							     if (jQuery('.widget-basket-totals').length <= 0) {
-							 	jQuery('.fa-shopping-cart').parent('button').click();
-							     } 
-							     clickResult({status: "SUCCESS"});
-							 window.location.href = jQuery('#nn_payment_process_url').val();
-							}
-						 	if(walletPayments[walletPayment] == 'novalnet_googlepay') {
-							    if (jQuery('.widget-basket-totals').length <= 0) {
-							 	jQuery('.fa-shopping-cart').parent('button').click();
-							     } 
-							     clickResult({status: "SUCCESS"});
-							} 
+					 // onPaymentButtonClicked: function(clickResult) {
+						// 	console.log('click');
+						// 	console.log(window.ceresStore.state);
+						// 	console.log(window.ceresStore.state.basket.item);
+						// 	var id = null;
+						// 	if (typeof window.ceresStore.state.item !== 'undefined' && window.ceresStore.state.item.variation.documents[0]) {
+						// 	id = window.ceresStore.state.item.variation.documents[0].data.variation.id;
+						// 	} else if (typeof window.ceresStore.state.items.mainItemId !== 'undefined' && window.ceresStore.state.items[window.ceresStore.state.items.mainItemId]) {
+						// 	id = window.ceresStore.state.items[window.ceresStore.state.items.mainItemId].variation.documents[0].data.variation.id;
+						// 	}
+						// 	if(walletPayments[walletPayment] == 'novalnet_applepay')  {
+						// 	     if (jQuery('.widget-basket-totals').length <= 0) {
+						// 	 	jQuery('.fa-shopping-cart').parent('button').click();
+						// 	     } 
+						// 	     clickResult({status: "SUCCESS"});
+						// 	 window.location.href = jQuery('#nn_payment_process_url').val();
+						// 	}
+						//  	if(walletPayments[walletPayment] == 'novalnet_googlepay') {
+						// 	    if (jQuery('.widget-basket-totals').length <= 0) {
+						// 	 	jQuery('.fa-shopping-cart').parent('button').click();
+						// 	     } 
+						// 	     clickResult({status: "SUCCESS"});
+						// 	} 
 							
-						},
+						// },
 					}
 				}
 			};
 			
 			// Checking for the Payment method availability
-
 			if((walletPayments[walletPayment] == "novalnet_googlepay") || (walletPayments[walletPayment] == "novalnet_applepay")) {
 				console.log(walletPayments[walletPayment]);
             			displayWalletButton(walletPayments[walletPayment]);
