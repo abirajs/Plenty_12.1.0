9934932766347559030b14e7ef4866c6e7acaf78
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-20 14:29:37 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -537,7 +537,7 @@ class PaymentController extends Controller
         $this->getLogger(__METHOD__)->error('Novalnet::$data', $data);
 	$this->sessionStorage->getPlugin()->setValue('nnExpressPaymentData',$data);
   	// return $this->response->redirectTo('checkout?readonlyCheckout=1');
-	return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
+	return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/place-order');
     }
 
 
