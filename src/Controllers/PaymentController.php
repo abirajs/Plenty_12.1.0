4a6d3f4ae6d4dc4e966c39333f10f684372f4890
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-21 10:40:31 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -370,7 +370,7 @@ class PaymentController extends Controller
     {
         // Get the payment form post data
         $paymentRequestPostData = $this->request->all();
-	$tttttttt =  $paymentRequestPostData['nn_google_pay_response'];
+	$tttttttt =  $paymentRequestPostData->additionalInfo;
 		
 	$this->getLogger(__METHOD__)->error('Novalnet::$paymentRequestPostData', $paymentRequestPostData);
 	$this->getLogger(__METHOD__)->error('Novalnet::$tttttttt', $tttttttt);
