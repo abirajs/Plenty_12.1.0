08b8038a3549327688aa091be25dfd7dc5f8293c
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-21 14:26:28 +0530

    Update NovalnetExpressCheckoutDataProvider.php

--- src/Providers/DataProvider/NovalnetExpressCheckoutDataProvider.php
+++ src/Providers/DataProvider/NovalnetExpressCheckoutDataProvider.php
@@ -89,7 +89,7 @@ class NovalnetExpressCheckoutDataProvider
 	     $configurationData = json_encode($configurationArr);
 	     $merchantId = $settingsService->getPaymentSettingsValue('payment_active', 'novalnet_googlepay');
 	     $isEnforceEnabled = $settingsService->getPaymentSettingsValue('enforce', 'novalnet_googlepay');
-             // $shippingMethod = $paymentHelper->getShippingProfileList();
+             $shippingMethod = $paymentHelper->getShippingProfileList();
 	     $this->getLogger(__METHOD__)->error('Novalnet::$shippingMethod', $shippingMethod);
              $this->getLogger(__METHOD__)->error('Novalnet::$configurationData', $configurationData);
              $this->getLogger(__METHOD__)->error('Novalnet::$paymentMethodDetails[0]', $paymentMethodDetails[0]);
