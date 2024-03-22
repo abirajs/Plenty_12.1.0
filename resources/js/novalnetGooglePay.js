a1ec048dc2f833ef83ac9583a95759f4a0bce156
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-22 11:21:34 +0530

    Update novalnetGooglePay.js

--- resources/js/novalnetGooglePay.js
+++ resources/js/novalnetGooglePay.js
@@ -145,7 +145,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }
 
-                if (jQuery('#nn_express_key').val() != '' && jQuery('input[type="radio"][id*='+mopId+']').is(':checked')) {
+                if ( jQuery('input[type="radio"][id*='+mopId+']').is(':checked')) {
                     console.log('nn_express_key');
                     jQuery('.fa-arrow-right').parent('button').hide();
                     jQuery("#nn_google_pay").show(); 
