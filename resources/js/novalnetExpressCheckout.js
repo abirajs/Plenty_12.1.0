2a56f9e0b0a821fd101b16a6e4987ebb46573d72
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-25 19:29:23 +0530

    Update novalnetExpressCheckout.js

--- resources/js/novalnetExpressCheckout.js
+++ resources/js/novalnetExpressCheckout.js
@@ -124,7 +124,8 @@ jQuery(document).ready(function() {
 						 // There could be a situation where the shipping method can alter total  
 						let transactionInfoToUpdate = {};
 						// Recalculating the total gross based on the chosen shipping method
-						transactionInfoToUpdate.amount = ( shippingMethod.amount * 100 ) + requestData.paymentIntent.transaction.amount;
+						// transactionInfoToUpdate.amount = ( shippingMethod.amount * 100 ) + requestData.paymentIntent.transaction.amount;
+						 transactionInfoToUpdate.amount = 6000;
 						newShippingMethodResult(transactionInfoToUpdate);
 					 },
 						
