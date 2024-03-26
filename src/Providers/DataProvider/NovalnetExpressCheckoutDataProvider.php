8d466541f209f4a03f2985c498827ac60bed2d75
abirajs <abiraj_s@novalnetsolutions.com>
2024-03-26 19:02:38 +0530

    Update NovalnetExpressCheckoutDataProvider.php

--- src/Providers/DataProvider/NovalnetExpressCheckoutDataProvider.php
+++ src/Providers/DataProvider/NovalnetExpressCheckoutDataProvider.php
@@ -108,14 +108,11 @@ class NovalnetExpressCheckoutDataProvider
 	 $shippingProfileId = json_encode($shippingProfileId);
 
 	 $availableShippingCountry = $countryRepository->getActiveCountriesList();
-	 $availableShippingCountry = json_decode(json_encode($availableShippingCountry));
+	 // $availableShippingCountry = json_decode(json_encode($availableShippingCountry));
 	 // $availableShippingCountry = json_decode($availableShippingCountry, true);
 
-	 $availableShippingCountry = (array) $availableShippingCountry;
-		
-	 $availableShippingCountry = stripslashes($availableShippingCountry);
-	 $availableShippingCountry = json_decode($availableShippingCountry, true);
-		
+	 // $availableShippingCountry = (array) $availableShippingCountry;
+
 	 $this->getLogger(__METHOD__)->error('Novalnet::$countryRepository', $availableShippingCountry);
          $this->getLogger(__METHOD__)->error('Novalnet::$shippingDetails', $shippingDetails);
 	 $this->getLogger(__METHOD__)->error('Novalnet::$shippingMethod', $shippingMethod);
