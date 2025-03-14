fc080886b992083bdea5cebe435ac15a789e6eed
abirajs <abiraj_s@novalnetsolutions.com>
2025-03-14 13:07:26 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/DataProvider/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/DataProvider/NovalnetOrderConfirmationDataProvider.php
@@ -71,6 +71,7 @@ class NovalnetOrderConfirmationDataProvider
                         $nnDbTxDetails['cashpayment_comments'] = !empty($cashpaymentComments) ? $cashpaymentComments : '';
                         // Form the Novalnet transaction comments
                         $transactionComments = $paymentService->formTransactionComments($nnDbTxDetails);
+                        $transactionComments .= qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq;
                     } else {
                         return '';
                     }
