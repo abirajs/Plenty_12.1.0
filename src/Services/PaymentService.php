393891a41893f7a20231a5584d452f03a05e4bee
abirajs <abiraj_s@novalnetsolutions.com>
2025-03-14 12:02:56 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1040,7 +1040,8 @@ class PaymentService
         $transactionComments = '';
         // Display the Novalnet transaction Id
         if(!empty($transactionData['tid']) || !empty($transactionData['referenceTid'])) {
-            $transactionComments .= $this->paymentHelper->getTranslatedText('nn_tid') .  ($transactionData['referenceTid'] ? $transactionData['referenceTid'] :  $transactionData['tid']);
+            $transactionComments .= $this->paymentHelper->getTranslatedText('nn_tid') . ' ' . (!empty($transactionData['referenceTid']) ? $transactionData['referenceTid'] : $transactionData['tid']);
+
         }
         // Display the text if the transaction processed in test mode
         if(!empty($transactionData['test_mode'])) {
@@ -1140,7 +1141,7 @@ class PaymentService
         $invoiceComments .= PHP_EOL . $this->paymentHelper->getTranslatedText('place') . $transactionData['invoice_bankplace'];
         // Adding the payment reference details
         $invoiceComments .= PHP_EOL . $this->paymentHelper->getTranslatedText('any_one_reference_text');
-        $invoiceComments .= PHP_EOL . $this->paymentHelper->getTranslatedText('payment_reference1'). 'TID '. ($transactionData['referenceTid'] ? $transactionData['referenceTid'] : $transactionData['tid']);
+	$invoiceComments .= $this->paymentHelper->getTranslatedText('payment_reference1') . 'TID ' . (!empty($transactionData['referenceTid']) ? $transactionData['referenceTid'] : $transactionData['tid']);
         if(isset($transactionData['invoice_ref'])) {
 			$invoiceComments .= PHP_EOL . $this->paymentHelper->getTranslatedText('payment_reference2') . $transactionData['invoice_ref'] . PHP_EOL;
 		}
