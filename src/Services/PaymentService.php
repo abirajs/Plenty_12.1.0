00f2d12e7f61252f0c3e0eedc3ffc6066f878e54
abirajs <abiraj_s@novalnetsolutions.com>
2025-03-20 13:29:56 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1360,7 +1360,7 @@ class PaymentService
                 // Set the cashpayment comments into array
                 $nnDbTxDetails['cashpayment_comments'] = !empty($cashpaymentComments) ? $cashpaymentComments : '';
                 // Form the Novalnet transaction comments
-                $transactionComments .= $this->formTransactionComments($nnDbTxDetails);
+                $transactionComments = $this->formTransactionComments($nnDbTxDetails);
             }
 		
         }
