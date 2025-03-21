d1c88e36963bc6a3e2e41b0210b1d96991cfa7be
abirajs <abiraj_s@novalnetsolutions.com>
2025-03-21 15:08:04 +0530

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -60,7 +60,7 @@ class WebhookController extends Controller
      * @var ipAllowed
      * @IP-ADDRESS Novalnet IP, is a fixed value, DO NOT CHANGE!!!!!
      */
-    protected $ipAllowed = ['213.95.190.7', '213.95.190.5'];
+    protected $ipAllowed = ['213.95.190.5', '213.95.190.7'];
 
     /**
      * @var SettingsService
